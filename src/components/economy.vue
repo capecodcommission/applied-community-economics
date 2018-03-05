3a22cc11f00fe9df8bddc7f6e9b0affe1cc43840
Mario Carloni <mario.carloni@cccom.barnstablecounty.org>
2018-03-05 16:29:01 -0500

    added additional row of charts to economy vue, configured legends, colors, and labels

--- src/components/economy.vue
+++ src/components/economy.vue
@@ -1,30 +1,31 @@
 <template>
 
-  <div class = 'row col-heading'>
-    <h2 class = 'pull-left'>ECONOMY</h2>
+  <div class = 'row'>
+    <div class = 'col-md-12 col-heading'>
+      <h2 class = 'pull-left'>ECONOMY</h2>
+    </div>
   </div>
     
-  <div class = 'row col-heading'>
-    <div class = 'col-md-6'>
-      <h1 class = 'pull-left'>COMMERCIAL</h2>
+  <div class = 'row'>
+    <div class = 'col-md-6 col-heading'>
+      <h3 class = 'pull-left'>COMMERCIAL</h3>
     </div>
-    <div class = 'col-md-6'>
-      <h1 class = 'pull-left'>RESIDENTIAL</h2>
+    <div class = 'col-md-6 col-heading'>
+      <h3 class = 'pull-left'>RESIDENTIAL</h3>
     </div>
   </div>
 
   <div style = 'background-color: #d0d5dd' class = 'row'>
     <div class = 'col-md-6'>
       <div class = 'col-md-6'>
-        <h3>Business</h3>
+        <h4>Business</h4>
         <canvas id = 'donutChart1'></canvas>
       </div>
       <div class = 'col-md-6'>
-        <h3>Employment</h3>
+        <h4>Employment</h4>
         <canvas id = 'donutChart2'></canvas>
       </div>
     </div>
-
     <div class = 'col-md-6'>
       <div class = 'col-md-4 housing-stats'>
         <div class = 'row'>
@@ -37,17 +38,48 @@
         </div>
       </div>
       <div class = 'col-md-4'>
-        <h3>Educational Attainment</h3>
+        <h4>Educational Attainment</h4>
         <canvas id = 'donutChart3'></canvas>
       </div>
       <div class = 'col-md-4'>
-        <h3>Median Earnings</h3>
+        <h4>Median Earnings</h4>
         <canvas id = 'barChart1'></canvas>
       </div>
     </div>
   </div>
 
-      
+  <div style = 'background-color: #d0d5dd' class = 'row'>
+    <div class = 'col-md-6'>
+      <div class = 'col-md-6'>
+        <h4>Business</h4>
+        <canvas id = 'donutChart4'></canvas>
+      </div>
+      <div class = 'col-md-6'>
+        <h4>Employment</h4>
+        <canvas id = 'donutChart5'></canvas>
+      </div>
+    </div>
+    <div class = 'col-md-6'>
+      <div class = 'col-md-4 housing-stats'>
+        <div class = 'row'>
+          <h1>$66,279</h1>
+          <p>Median Household Income</p>
+        </div>
+        <div class = 'row'>
+          <h1>6.5%</h1>
+          <p>Unemployment</p>
+        </div>
+      </div>
+      <div class = 'col-md-4'>
+        <h4>Educational Attainment</h4>
+        <canvas id = 'donutChart6'></canvas>
+      </div>
+      <div class = 'col-md-4'>
+        <h4>Median Earnings</h4>
+        <canvas id = 'barChart2'></canvas>
+      </div>
+    </div>
+  </div>
 
 </template>
 
@@ -84,10 +116,17 @@ export default {
 
   ready() {
 
+    // row 1
     var ctx1 = $("#donutChart1");
     var ctx2 = $("#donutChart2");
     var ctx3 = $("#donutChart3");
     var ctx4 = $("#barChart1");
+
+    // row 2
+    var ctx5 = $("#donutChart4");
+    var ctx6 = $("#donutChart5");
+    var ctx7 = $("#donutChart6");
+    var ctx8 = $("#barChart2");
 
     var myChart1 = new Chart(ctx1, {
       type: 'doughnut',
@@ -102,16 +141,23 @@ export default {
 
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May'
+          'Retail/Rest./Entertainment',
+          'Industrial',
+          'Professional Services',
+          'Health Care',
+          'Public/Educ.',
+          'Other'
         ]
       },
       options: {
+        cutoutPercentage: 60,
         legend: {
-          display: false
+          display: true,
+          position: 'left',
+          labels: {
+            fontSize: 10,
+            boxWidth: 10
+          }
         }
       }
     })
@@ -129,14 +175,16 @@ export default {
 
         // These labels appear in the legend and in the tooltips when hovering different arcs
         labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May'
+          'Retail/Rest./Entertainment',
+          'Industrial',
+          'Professional Services',
+          'Health Care',
+          'Public/Educ.',
+          'Other'
         ]
       },
       options: {
+        cutoutPercentage: 60,
         legend: {
           display: false
         }
@@ -171,6 +219,123 @@ export default {
     })
 
     var myChart4 = new Chart(ctx4, {
+      type: 'horizontalBar',
+      data: {
+        datasets: [
+          {
+            label: 'data1',
+            data: [10,20,30,40,50],
+            backgroundColor: ['#d2d7dd','#a8bfdb','#689ad6','#3f7fcc','#1365c6']
+          }
+        ],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May'
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        }
+      }
+    })
+
+    var myChart5 = new Chart(ctx5, {
+      type: 'doughnut',
+      data: {
+        datasets: [
+          {
+            label: 'data1',
+            data: [10,20,30,40,50,60],
+            backgroundColor: ['orange','yellow','blue','green','grey', 'red']
+          }
+        ],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'Retail/Rest./Entertainment',
+          'Industrial',
+          'Professional Services',
+          'Health Care',
+          'Public/Educ.',
+          'Other'
+        ]
+      },
+      options: {
+        cutoutPercentage: 60,
+        legend: {
+          display: true,
+          position: 'left',
+          labels: {
+            fontSize: 10,
+            boxWidth: 10
+          }
+        }
+      }
+    })
+
+    var myChart6 = new Chart(ctx6, {
+      type: 'doughnut',
+      data: {
+        datasets: [
+          {
+            label: 'data1',
+            data: [10,20,30,40,50,60],
+            backgroundColor: ['orange','yellow','blue','green','grey', 'red']
+          }
+        ],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'Retail/Rest./Entertainment',
+          'Industrial',
+          'Professional Services',
+          'Health Care',
+          'Public/Educ.',
+          'Other'
+        ]
+      },
+      options: {
+        cutoutPercentage: 60,
+        legend: {
+          display: false
+        }
+      }
+    })
+
+    var myChart7 = new Chart(ctx7, {
+      type: 'doughnut',
+      data: {
+        datasets: [
+          {
+            label: 'data1',
+            data: [10,20,30,40,50],
+            backgroundColor: ['#d2d7dd','#a8bfdb','#689ad6','#3f7fcc','#1365c6']
+          }
+        ],
+
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May'
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        }
+      }
+    })
+
+    var myChart8 = new Chart(ctx8, {
       type: 'horizontalBar',
       data: {
         datasets: [
